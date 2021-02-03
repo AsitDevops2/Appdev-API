429fe4a8b3d92e568296a7b4154ca7b73e08c086
Abhay Thakur <abhay.thakur@jadeglobal.com>
2021-02-03 10:59:43 +0530

    add logout controller

--- src/main/java/com/raley/controller/LogoutController.java
+++ src/main/java/com/raley/controller/LogoutController.java
@@ -1,8 +1,13 @@
 package com.raley.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author abhay.thakur
  *
  */
 public class LogoutController {
+	Logger logger = LoggerFactory.getLogger(LogoutController.class);
 
 }
