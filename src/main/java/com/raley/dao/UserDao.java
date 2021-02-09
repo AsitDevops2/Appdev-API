7a53c4a7ae9316b47a53d6eefd3c6243e20de0c9
AsitDevops2 <77683148+AsitDevops2@users.noreply.github.com>
2021-02-09 19:59:05 +0530

    Update UserDao.java

--- src/main/java/com/raley/dao/UserDao.java
+++ src/main/java/com/raley/dao/UserDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.raley.model.User;
-
+//Asit
 @Repository
 public interface UserDao extends CrudRepository<User, Integer> {
 
